20cfdf2ed0405d834f39ce19fce38b7e3500734c
Andrew Quaye <andyquayegh@outlook.com>
2019-05-22 15:37:24 +0100

    update
    correct line code ith seesion

--- modules/enrollment.php
+++ modules/enrollment.php
@@ -11,33 +11,73 @@ class ENROLL{
     function  add_enrollment($conn){
 
         $date = $_POST['date'];
-        $student= $_SESSION['student_index'];
+        $student= $_SESSION['student_index_id'];
         $year = $_POST['year'];
         $semester = $_POST['semester'];
         $programme = $_POST['programme'];
         $level = $_POST['level'];
         $pin = $_POST['pin'];
 
-        $sql ="SELECT * FROM `get_enrollment` where pins='$pin' AND stud_index='$student' AND s_level='$level' AND semesterID='$semester' AND statusID ='1'";
+        //get student
+        $sql ="SELECT * FROM `get_student_index` where `stud_indexID`='$student' LIMIT 0, 1000";
         $result = mysqli_query($conn,$sql);
         if ($result->num_rows == 0){
-            header("location: index.php?_route=student&p=enrollment.form&e=102");
-        }else {
-            $sql = "UPDATE `enrollment` SET
-                  `enroll_date` = '$date',
-                  `yearID` = '$year', 
-                  `progID` = '$programme',
-                  `statusID` = '2' 
-                  WHERE `pins` = '$pin'";
-            $result = mysqli_query($conn, $sql);
-            if ($result == TRUE) {
-
-                header("location: index.php?_route=student&p=enrollment.form&e=104");
-            } else {
-                header("location: index.php?_route=student&p=enrollment.form&e=103");
-                exit();
-            }
+            $student =0;
+        }else{
+            $r = $result->fetch_assoc();
+            $catID = $r['categoryID'];
         }
+
+        // get payment
+        $sql ="SELECT * FROM `get_total_payment` where `studentID`='$student' AND `progID`='$programme' AND `semesterID`='$semester' AND `levelID`='$level' AND `yearID`='$year'";
+        $result = mysqli_query($conn,$sql);
+        if ($result->num_rows == 0){
+            $paid_amount =0;
+        }else{
+            $r = $result->fetch_assoc();
+            $paid_amount = $r['paid'];
+        }
+
+
+        // get fees
+        $sql ="SELECT * FROM `get_fee_prices_list` where `progID`='$programme' AND `catID`='$catID' AND `semesterID`='$semester'";
+        $result = mysqli_query($conn,$sql);
+        if ($result->num_rows == 0){
+            $bill_amount =0;
+        }else{
+            $r = $result->fetch_assoc();
+            $bill_amount = $r['amount'];
+
+            $bill = ($bill_amount *(75/100));
+        }
+
+        if($paid_amount > $bill){
+
+            $sql ="SELECT * FROM `get_enrollment` where pins='$pin' AND stud_index='$student' AND s_level='$level' AND semesterID='$semester' AND statusID ='1'";
+            $result = mysqli_query($conn,$sql);
+            if ($result->num_rows == 0){
+                header("location: index.php?_route=student&p=enrollment.form&e=102");
+            }else {
+                $sql = "UPDATE `enrollment` SET `enroll_date` = '$date',`yearID` = '$year',`progID` = '$programme',`statusID` = '2' WHERE `pins` = '$pin'";
+                $result = mysqli_query($conn, $sql);
+                if ($result == TRUE) {
+
+                    header("location: index.php?_route=student&p=enrollment.form&e=104");
+                } else {
+                    header("location: index.php?_route=student&p=enrollment.form&e=103");
+                    exit();
+                }
+            }
+
+        }else{
+            header("location: index.php?_route=student&p=enrollment.form&e=128");
+            exit();
+        }
+
+
+
+
+
     }
 
     function add_reg_course($conn){
